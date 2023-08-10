198f766d4cda21824bbe61731e797d8795d26af9
Олег Смаранди <Oleg Smarandi>
2023-08-10 10:45:47 +0300

    DD-4 | Add users table and setup database

--- data_dime_user_auth/settings.py
+++ data_dime_user_auth/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from data_dime_user_auth import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,15 @@ WSGI_APPLICATION = 'data_dime_user_auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5416',
     }
 }
+
 
 
 # Password validation
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    subscription_status = models.CharField(
+        max_length=20, choices=[('free', 'Free'), ('paid', 'Paid')]
+    )
+
+    def __str__(self):
+        return self.username

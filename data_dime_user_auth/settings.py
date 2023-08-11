c46a1ba95bb587f48427148710d2c41da8c62f37
Олег Смаранди <Oleg Smarandi>
2023-08-11 07:57:28 +0300

    DD-4 | Fixing security issues

--- data_dime_user_auth/settings.py
+++ data_dime_user_auth/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a_!300+@&o*ccl9%$-as^u-*ads#$rf_u6q1u4rq=xwvhg7pyl'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

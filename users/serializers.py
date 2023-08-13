8eb62cde539cad9f3cde093ab1112ab4371f595d
Олег Смаранди <Oleg Smarandi>
2023-08-13 11:24:35 +0300

    DD-5 | User Registration and Login implementation

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
-
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = [
-            'username',
-            'email',
-            'subscription_status'
-        ]
+class UserRegistrationSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=30)
+    password = serializers.CharField(write_only=True)  # Ensure password is write-only
+    email = serializers.EmailField()
+    subscription_status = serializers.ChoiceField(choices=['free', 'paid'], default='free')
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=30)
+    password = serializers.CharField(write_only=True)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User as DjangoUser
-from django.contrib.auth.hashers import make_password
-from django.http import HttpRequest
 import json
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password, check_password
+from django.http import HttpRequest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from .models import User
 
 
@@ -15,36 +15,31 @@ def api_home(request, *args, **kwargs):
 
 @api_view(["POST"])
 def api_register_user(request: HttpRequest, *args, **kwargs):
-    payload = json.loads(request.body)
+    serializer = UserRegistrationSerializer(data=json.loads(request.body))
+    if serializer.is_valid():
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+        email = serializer.validated_data['email']
+        subscription_status = serializer.validated_data['subscription_status']
 
-    username_exists = User.objects.filter(username=payload['username']).exists()
-    email_exists = User.objects.filter(email=payload['email']).exists()
+        user = User.objects.create(username=username,
+                                   password=make_password(password),
+                                   email=email,
+                                   subscription_status=subscription_status)
 
-    if not (username_exists or email_exists):
-        hashed_password = make_password(payload['password'])
-        new_user = User.objects.create(
-            username=payload['username'],
-            password=hashed_password,
-            email=payload['email'],
-            subscription_status='free'
-        )
-
-        DjangoUser.objects.create_user(username=payload['username'], email=payload['email'], password=hashed_password)
-        return Response(status=201)
-    else:
-        return Response({"message": "Username or email already exists!"}, status=400)
+        return Response({"message": "User registered successfully"}, status=201)
+    return Response(serializer.errors, status=400)
 
 
 @api_view(["POST"])
 def api_login_user(request: HttpRequest, *args, **kwargs):
-    payload = json.loads(request.body)
-    username = payload.get('username')
-    password = payload.get('password')
-
-    user = authenticate(request, username=username, password=password)
-
-    if user is not None:
-        login(request, user)
-        return Response({"message": "Login successful!"}, status=200)
-    else:
+    serializer = UserLoginSerializer(data=json.loads(request.body))
+    if serializer.is_valid():
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+        user = User.objects.filter(username=username).first()
+        user_found = user is not None
+        if user_found and check_password(password, user.password):
+            return Response({"message": "Login successful"}, status=200)
         return Response({"message": "Invalid credentials"}, status=401)
+    return Response(serializer.errors, status=400)
